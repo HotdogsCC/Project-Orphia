485567994ffa13faf0a8131e90ebc846f83f4028
Charlie Campbell <chacha9669@hotmail.com>
2024-04-26 13:30:05 +1000

    Wasp progress

---
+++ Assets/Scripts/Enemy/WaspSpit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaspSpit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
