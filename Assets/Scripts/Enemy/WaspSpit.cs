eae6a3c1bf7f1bdcd16bc684d05743c0de9e9641
Charlie Campbell <chacha9669@hotmail.com>
2024-04-26 16:45:37 +1000

    Wasp Spit Progress

--- Assets/Scripts/Enemy/WaspSpit.cs
+++ Assets/Scripts/Enemy/WaspSpit.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class WaspSpit : MonoBehaviour
 {
+    private PlayerMovement player;
+    private GameObject parent;
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("spitting");
+            Debug.Log(Vector2.Angle(transform.position, player.transform.position));
+        }
         
     }
 }
